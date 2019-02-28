c7a05acc4a44f8067d62bcbf11d6f3ac5ccd20d8
Isabella Pham <isabellatheyellowamerican@gmail.com>
2019-02-27 22:00:22 -0500

    closed file

--- ProcessingIngredients.py
+++ ProcessingIngredients.py
@@ -10,35 +10,36 @@ def percentage(processed, total): #returns the percentage of ingredients that do
     return (processed/total)*100 + '%'
 
 def listNDBNO(list): #returns a dictionary of the food and it's NDBNO number
-    NDBNOs = {};
-    manually = [];
+    #NDBNOs = {};
+    #manually = [];
     total = 0;
     processed = 0;
+    f = open('Output.txt', 'w')
+    m = open('Manual.txt', 'w')
+    f.write('Below is a list of foods and their NDBNO numbers')
+    m.write('Below is a list of foods that must be manually checked')
     for food in list:
+        total = total + 1
         data = get_api(food)
         if data == 'ERROR':
-            manually.append(food)
+            m.write(food + '\n')
+            #manually.append(food)
             continue
         freqs = getFrequencies(data)
         if bool(freqs) == False:
             continue
         foodGroup = getHighestFreq(freqs)
         if isEntropyLow(freqs):
-            manually.append(food)
+            #manually.append(food)
+            m.write(food + '\n')
         else:
             number = getNDBNO(foodGroup, data);
-            NDBNOs[food] = number
-            processed += 1
-        total += 1
-        time.sleep(5)
-    print(percentage(processed/total))
-    f = open('Output.txt', 'w')
-    f.write('Below is a list of foods and their NDBNO numbers')
-    for food in NBDNOs:
-        f.write(food + ':' + NBDNOs[food] + '\n');
-    m = open('Manual.txt', 'w')
-    m.write('Below is a list of foods that must be manually checked')
-    for food in manually:
-        m.write(food + '\n')
+            #NDBNOs[food] = number
+            f.write(food + ':' + number + '\n');
+            processed = processed + 1
+        time.sleep(10)
+    f.write('Percentage of successful cases:' + percentage(processed/total))
+    f.close()
+    m.close()
 
 listNDBNO(getFood())
