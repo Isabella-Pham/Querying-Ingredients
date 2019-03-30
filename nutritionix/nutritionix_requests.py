68d19ac70a5c31c99f60bd8b1dd680d33cd04bcf
Isabella Pham <isabella.aivy.pham@gmail.com>
2019-03-29 21:18:29 -0400

    conversion tables

--- nutritionix/nutritionix_requests.py
+++ nutritionix/nutritionix_requests.py
@@ -2,8 +2,8 @@ import requests
 import json
 import os
 import time
+import urllib
 
-'''
 def getFood():
     with open ("list.txt", "r") as f:
         l = f.read().splitlines()
@@ -14,14 +14,56 @@ headers = {'Content-Type': 'application/json',
            "x-app-id": "240c70ea", 'x-app-key': '5b98e8dfb93416ccf82a05f5f5073f72', 'x-remote-user-id': '0'}
 
 
-data = '{ \"num_servings\": 1,\"query\": \"mango\",  \"aggregate\": \"string\",  \"line_delimited\": false,  \"use_raw_foods\": false,  \"include_subrecipe\": false,  \"timezone\": \"US\/ Eastern\",  \"consumed_at\": null,  \"lat\": 0,  \"lng\": 0,  \"meal_type\": 0,  \"use_branded_foods\": false,  \"locale\": \"en_US\"}'
+data = '{ \"num_servings\": 1,\"query\": \"mango\",  \"aggregate\": \"string\",  \"line_delimited\": false,  \"use_raw_foods\": true,  \"include_subrecipe\": false,  \"timezone\": \"US\/ Eastern\",  \"consumed_at\": null,  \"lat\": 0,  \"lng\": 0,  \"meal_type\": 0,  \"use_branded_foods\": true,  \"locale\": \"en_US\"}'
 
 json_data = json.loads(data)
+json_data["query"] = "pear"
+#headers['x-remote-user-id']=str(i%2)
+#print("Request:\n\n"+json.dumps(json_data, indent=2, sort_keys=True))
+response = requests.post(url, data=json.dumps(json_data), headers=headers)
+#write this to a file
+parsed = json.loads(response.text)
+# not food or no result
+if 'message' in parsed:
+    print(response.text)
+else:
+    print(response.text)
+
+'''
 outfile=open('nutritionix.txt','w')
 l=getFood()
 
 
+
 # remember we are limited to 2*500 queries a day, Isabella you have an offset 1229
+with open ("noResults.txt", "r") as f:
+    l = f.read().splitlines()
+outfile = open('noResultsConversionTables.txt', 'w')
+for line in l:
+    if(line.split()[1].isdigit()):
+        NDBNO = line.split()[1]
+        url1 = 'https://api.nal.usda.gov/ndb/reports/?ndbno=' + NDBNO + '&type=f&format=json&api_key=adBdNNK4tcs1RKpEOJnb6If4pZ9YH1B75ii9roZS'
+        with urllib.request.urlopen(url1) as url1:
+            data = json.loads(url1.read().decode())
+        name =  data['report']['food']['name']
+        json_data["query"] = name
+    	#headers['x-remote-user-id']=str(i%2)
+    	#print("Request:\n\n"+json.dumps(json_data, indent=2, sort_keys=True))
+        response = requests.post(url, data=json.dumps(json_data), headers=headers)
+    	#write this to a file
+        parsed = json.loads(response.text)
+    	# not food or no result
+        if 'message' in parsed:
+            outfile.write(line.split()[0]+'\t0\t'+response.text+'\n')
+        else:
+            outfile.write(line.split()[0]+'\t'+str(len(parsed))+'\t'+response.text+'\n')
+        outfile.flush()
+    else:
+        outfile.write(line + "\n")
+        outfile.flush()
+    time.sleep(10)
+outfile.close()
+
 for i in l[1229:]:
 	# way to change query
 	print(i)
@@ -64,4 +106,18 @@ def getNoResults():
             outfile.flush()
     outfile.close()
 
-getNoResults()
+def noResultsConverstionTable():
+    with open ("noResults.txt", "r") as f:
+        l = f.read().splitlines()
+    outfile = open('noResultsConversionTables.txt', 'w')
+    for line in l:
+        if(line.split()[1].isdigit()):
+            NDBNO = line.split()[1]
+            url = 'https://api.nal.usda.gov/ndb/reports/?ndbno=' + NDBNO + '&type=f&format=json&api_key=adBdNNK4tcs1RKpEOJnb6If4pZ9YH1B75ii9roZS'
+            with urllib.request.urlopen(url) as url:
+                data = json.loads(url.read().decode())
+            name =  data['report']['food']['name']
+        else:
+            outfile.write(line + "\n")
+            outfile.flush()
+    outfile.close()
